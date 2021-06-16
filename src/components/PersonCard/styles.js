21e9e6932ad03c2c6f895a3f9ee7325c2550d104
Kely <kelly_le_23mai@hotmail.com>
2021-06-16 11:16:37 -0300

    commit styles

--- src/components/PersonCard/styles.js
+++ src/components/PersonCard/styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Img = styled.img`
-    width: 140px;
-    height: 100px;
+    width: 250px;
+    height: 200px;
     border-radius: 3px;
     border: 2px solid black;
     margin: 5px;
